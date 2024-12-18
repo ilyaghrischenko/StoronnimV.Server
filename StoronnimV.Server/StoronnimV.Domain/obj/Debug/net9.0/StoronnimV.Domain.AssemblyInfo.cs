a58ab2512f5d705985484edef502579cbd5c4315
ilyaghrischenko <144327680+ilyaghrischenko@users.noreply.github.com>
2024-12-18 19:56:15 +0200

    finish members data getting

--- StoronnimV.Server/StoronnimV.Domain/obj/Debug/net9.0/StoronnimV.Domain.AssemblyInfo.cs
+++ StoronnimV.Server/StoronnimV.Domain/obj/Debug/net9.0/StoronnimV.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoronnimV.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea20d77cb807ecf73150b61da99c7daf0c3972a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03a93daee6ffd11706dc88cfc0432eda9a24c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoronnimV.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoronnimV.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
