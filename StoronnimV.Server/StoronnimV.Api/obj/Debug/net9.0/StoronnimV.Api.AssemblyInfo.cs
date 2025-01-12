994f3873e340a8916652564543c01088d89384bd
ilyaghrischenko <144327680+ilyaghrischenko@users.noreply.github.com>
2025-01-12 19:00:36 +0200

    solution build

--- StoronnimV.Server/StoronnimV.Api/obj/Debug/net9.0/StoronnimV.Api.AssemblyInfo.cs
+++ StoronnimV.Server/StoronnimV.Api/obj/Debug/net9.0/StoronnimV.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoronnimV.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919b359c20666193a250088d4f479ce1f55acc4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42144ec0b6341f134abc501f4a34b39997c2467")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoronnimV.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoronnimV.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
