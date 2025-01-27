09287ccaa21776806707cb1bdd6043a25216cc75
ilyaghrischenko <144327680+ilyaghrischenko@users.noreply.github.com>
2025-01-27 18:49:22 +0200

    commit

--- StoronnimV.Server/StoronnimV.Api/obj/Debug/net9.0/StoronnimV.Api.AssemblyInfo.cs
+++ StoronnimV.Server/StoronnimV.Api/obj/Debug/net9.0/StoronnimV.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoronnimV.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4001b092f7e44692ec292058a8031a632144f414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebd631e4058bf32e3be5ec895194e96ab190fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoronnimV.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoronnimV.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
